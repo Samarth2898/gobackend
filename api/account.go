d888708358496a37bdf9a9cb656342fdc415af31
Samarth <samarthaasoori@gmail.com>
2022-07-28 12:12:16 +0530

    added get account by ID

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	db "github.com/Samarth2898/gobackend/db/sqlc"
@@ -29,6 +30,30 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	if err!=nil {
 		ctx.JSON(http.StatusInternalServerError, errResponse(err))
 		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+
+type getAccountRequest struct {
+	ID	int64	`uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err!=nil {
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+		return 
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err!=nil{
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errResponse(err))	
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return	
 	}
 	ctx.JSON(http.StatusOK, account)
 }
